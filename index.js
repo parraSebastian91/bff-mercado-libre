9ef3e64b61f2b02a79547feae8f4ba7deecc86cb
Sebastian Parra <sebastian.parra@externos.bci.cl>
2021-02-06 19:57:58 -0300

    servicios listos

--- index.js
+++ index.js
@@ -17,20 +17,76 @@ app.get('/', (req, res) => {
 /**
  * Busqueda de item por query string
  */
-app.get('/api/item', (req, res) => {
+app.get('/api/item', async(req, res) => {
     const query = req.query.q;
-    // const respuesta = getSerch(query);
+    const reqURL = `https://api.mercadolibre.com/sites/MLA/search?q=${query}`;
+    const resultadoBusqueda = await axios.get(reqURL)
+        .then(response => {
+            const responseFormateado = {
+                author: {
+                    name: 'sebastian',
+                    lastname: 'parra'
+                },
+                categories: [
+                    'Categoria 1',
+                    'Categoria 2',
+                ],
+                items: []
+            }
+            responseFormateado.items = response.data.results.slice(1, 5).map(m => {
+                return {
+                    id: m.id,
+                    title: m.title,
+                    price: {
+                        currency: m.currency_id,
+                        amount: m.price,
+                        decimals: 0
+                    },
+                    picture: m.thumbnail,
+                    condition: m.attributes.find(f => f.id === 'ITEM_CONDITION').value_name,
+                    free_shipping: m.shipping.free_shipping
+                }
 
-    axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
-        .then(res => {
-            // console.log(res.data);
-            res.send(JSON.stringify(res))
+            });
+            res.send(responseFormateado)
         })
         .catch(error => {
-            // console.log(error);
-            res.send({})
+            console.log(error);
+            res.send(JSON.parse(error))
         });
-    
+
+})
+
+app.get('/api/item/:id', async(req, res) => {
+    const idItem = req.params.id;
+    const itemURL = `https://api.mercadolibre.com/items/${idItem}`;
+    const itemDescriptionURL = `https://api.mercadolibre.com/items/${idItem}/description`;
+    axios.all([
+            axios.get(itemURL),
+            axios.get(itemDescriptionURL)
+        ])
+        .then(axios.spread((itemResponse, descResponse) => {
+            console.log()
+            respItem = itemResponse.data;
+            respDesc = descResponse.data;
+            console.log(respDesc)
+            const responseFormateado = {
+                id: idItem,
+                title: respItem.title,
+                price: {
+                    currency: respItem.currency_id,
+                    amount: respItem.price,
+                    decimals: 0
+                },
+                picture: respItem.pictures[0].url,
+                condition: respItem.attributes.find(f => f.id === 'ITEM_CONDITION').value_name,
+                free_shipping: respItem.shipping.free_shipping,
+                sold_quantity: respItem.sold_quantity,
+                description: respDesc.plain_text
+            }
+            res.send(responseFormateado)
+
+        }))
 })
 
 app.listen(port, () => {
